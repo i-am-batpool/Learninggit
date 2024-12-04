2431f9184d3ddf19be59541c673eb34e586a2509
i-am-batpool <advait.physics@gmail.com>
2024-12-05 04:11:21 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,4 +4,5 @@ int main(void)
 {
     std::cout << "Hi!, you have reached the first code that I have added to this trial repo!!!\n";
     std::cout << "Hi!, this is the first time I'm making a change in a code that I have already uploaded in Github :)\n";
+    std::cout << "The third change, this one I made directly on github and will pull in local clone using 'git clone'\n";
 }
