55b6c235aef1274b358fc1bad2f8b25f0568d1d2
i-am-batpool <advait.physics@gmail.com>
2024-12-05 04:05:39 +0530

    Added a line to intro code

--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,5 @@
 int main(void)
 {
     std::cout << "Hi!, you have reached the first code that I have added to this trial repo!!!\n";
+    std::cout << "Hi!, this is the first time I'm making a change in a code that I have already uploaded in Github :)\n";
 }
